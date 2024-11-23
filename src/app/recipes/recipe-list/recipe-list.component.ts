67d63af00027200c11da41948082057d6a9a7bc4
Esraa Said <esraa195182@feng.bu.edu.eg>
2024-11-23 15:54:09 +0200

    basics

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { RecipeItemComponent } from "./recipe-item/recipe-item.component";
+import { Recipe } from '../recipe.model';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-recipe-list',
+  standalone: true,
+  imports: [RecipeItemComponent, CommonModule],
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent  implements OnInit{
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://th.bing.com/th/id/R.c423b4d3de84880e25e89471298c4913?rik=G7jqL1QyH3BrRg&pid=ImgRaw&r=0'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://th.bing.com/th/id/R.c423b4d3de84880e25e89471298c4913?rik=G7jqL1QyH3BrRg&pid=ImgRaw&r=0')
+  ];
+  ngOnInit(): void {
+  }
+}
